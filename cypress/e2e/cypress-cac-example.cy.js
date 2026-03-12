b9c30a537a087ffba210293555418f3475fb1784
Rafael Silva <rafalp286@gmail.com>
2026-03-12 10:26:54 -0300

    Refatorando código reduzindo as repetições

--- cypress/e2e/cypress-cac-example.cy.js
+++ cypress/e2e/cypress-cac-example.cy.js
@@ -1,61 +1,76 @@
 import { RequiredFields } from "./required-fields"
 
-const requireFields = new RequiredFields()
+const form = new RequiredFields()
 
-describe('Testando cac com cypress', () => {
+describe('Testando formulario cac com cypress', () => {
   beforeEach(() => {
     cy.visit('/src/index.html')
   })
 
-  it('preenche os campos obrigatórios e envia o formulário', () => {
-    requireFields.typeFirstName('Rafael')
-    requireFields.typeLastName('silva')
-    requireFields.typeEmail('rafaelteste@teste.com')
-    requireFields.typeFeedbackField('feedbackfield','testando campo obrigatorio')
+  const validInput = {
+    name: 'Rafael',
+    lastname: 'silva',
+    email: 'rafaelteste@teste.com',
+    feedbackfield: 'testando campo obrigatorio'
+  }
 
-    requireFields.clickButton()
-    cy.get('.success').should('be.visible')
+
+  const invalidInput = {
+    name: '',
+    lastname: '',
+    email: '',
+    feedbackfield: ''
+  }
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+      form.typeFirstName(validInput.name)
+      form.typeLastName(validInput.lastname)
+      form.typeEmail(validInput.email)
+      form.typeFeedbackField(validInput.feedbackfield)
+
+      form.clickButton()
+      form.validationAlert('successalert')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+      form.typeFirstName(validInput.name)
+      form.typeLastName(validInput.lastname)
+      form.typeEmail('rafaelteste')
+      form.typeFeedbackField(validInput.feedbackfield)
+
+      form.clickButton()
+      form.validationAlert('erroralert')
+    })
+
+    it('validar se ao digitar valor *não numérico* no campo de telefone campo continue *vazio*', () => {
+      cy.get('#phone').type('teste campo numerico')
+      cy.get('#phone').should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', () => {
+      form.typeFirstName(validInput.name)
+      form.typeLastName(validInput.lastname)
+      form.typeEmail(validInput.email)
+      form.typeFeedbackField(validInput.feedbackfield)
+
+      cy.get('#phone-checkbox').check()
+      form.clickButton()
+      form.validationAlert('erroralert')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+      form.typeFirstName(validInput.name)
+      form.clearField('firstname')
+      form.typeLastName(validInput.lastname)
+      form.clearField('lastname')
+      form.typeEmail('rafaelteste')
+      form.clearField('email')
+      form.typeFeedbackField(validInput.feedbackfield)
+      form.clearField('feedbackfield')     
+    })
+    
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+      form.clickButton()
+      form.validationAlert('erroralert')
+    })
   })
-
-  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
-    requireFields.typeFirstName('Rafael')
-    requireFields.typeLastName('silva')
-    requireFields.typeEmail('rafaelteste')
-    requireFields.typeFeedbackField('feedbackfield','testando campo obrigatorio')
-
-    requireFields.clickButton()
-    cy.get('.error').should('be.visible')
-  })
-
-  it('validar se ao digitar valor *não numérico* no campo de telefone campo continue *vazio*', () => {
-    cy.get('#phone').type('teste campo numerico')
-    cy.get('#phone').should('have.value', '')
-  })
-
-  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido', () => {
-    requireFields.typeFirstName('Rafael')
-    requireFields.typeLastName('silva')
-    requireFields.typeEmail('rafaelteste@teste.com')
-    requireFields.typeFeedbackField('testando campo obrigatorio')
-
-    cy.get('#phone-checkbox').check()
-    requireFields.clickButton()
-    cy.get('.error').should('be.visible')
-  })
-
-  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
-    requireFields.typeFirstName('Rafael')
-    requireFields.clearField('firstname')
-    requireFields.typeLastName('silva')
-    requireFields.clearField('lastname')
-    requireFields.typeEmail('rafaelteste')
-    requireFields.clearField('email')
-    requireFields.typeFeedbackField('testando campo obrigatorio')
-    requireFields.clearField('feedbackfield')     
-  })
-  
-  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-    requireFields.clickButton()
-    cy.get('.error').should('be.visible')
-  })
-})
--- cypress/e2e/required-fields.js
+++ cypress/e2e/required-fields.js
@@ -5,32 +5,32 @@ export class RequiredFields {
         lastname: () => cy.get('#lastName'),
         email: () => cy.get('#email'),
         feedbackfield: () => cy.get('#open-text-area'),
-        submitbutton: () => cy.get('.button')
+        submitbutton: () => cy.get('.button'),
+        successalert: () => cy.get('.success'),
+        erroralert: () => cy.get('.error')
+
     }
 
     typeFirstName(text) {
         this.elements.firstname().type(text)
     }
-    
     typeLastName(text) {
         this.elements.lastname().type(text)
     }
-
     typeEmail(text) {
         this.elements.email().type(text)
     }
-
     typeFeedbackField(text) {
         this.elements.feedbackfield().type(text)
     }
-
-
     clearField(field) {
         this.elements[field]().clear().should('have.value', '')
     }
-
     clickButton() {
         this.elements.submitbutton().click()
+    }
+    validationAlert(field) {
+        this.elements[field]().should('be.visible')
     }
     
 }
